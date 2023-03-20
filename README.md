6aee2bfc3fcaaa56f752305d346dea5f9eeb4f19
Soe Moe <hi@soe.moe>
2023-03-20 13:06:34 +0630

    chore: update README

---
+++ README.md
@@ -0,0 +1,26 @@
+# @bigbee.dev/expo-detect-mm-encoding
+
+detect Myanmar text encoding
+
+# Installation 
+
+```
+npm install @bigbee.dev/expo-detect-mm-encoding
+```
+
+Or 
+```
+yarn add @bigbee.dev/expo-detect-mm-encoding 
+```
+
+##  Configure for iOS
+
+Run `npx pod-install` after installing the npm package.
+
+# API Usage
+
+```Typescript
+import { isUnicode } from '@bigbee.dev/expo-detect-mm-encoding';
+
+const uni = await isUnicode();
+```
--- example/App.tsx
+++ example/App.tsx
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import * as DetectMMEncoding from '@bigbee.dev/expo-detect-mm-encoding';
+import { isUnicode } from '@bigbee.dev/expo-detect-mm-encoding';
 
 export default function App() {
   useEffect(() => {
     const start = performance.now();
-    DetectMMEncoding.isUnicode();
+    isUnicode();
     const end = performance.now();
     console.log(`DetectMMEncoding.isUnicode() took ${end - start} ms`);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>{`Unicode is: ${DetectMMEncoding.isUnicode()}`}</Text>
+      <Text>{`Unicode is: ${isUnicode()}`}</Text>
     </View>
   );
 }
